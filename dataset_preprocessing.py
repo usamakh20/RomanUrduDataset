6deaabf8eaf85358563d7d88dc8a7b3b33a31c98
usamathescientist <usamathescientist@gmail.com>
2020-08-25 17:11:55 +0500

    Initial commit

--- dataset_preprocessing.py
+++ dataset_preprocessing.py
@@ -1,8 +1,8 @@
 import re
 import time
+import os
 import numpy as np
 import pandas as pd
-from collections import Counter
 from helper import transliterate
 
 data_dir = 'Urdu datasets/'
@@ -44,27 +44,27 @@ def sub_nextline(string):
 
 
 # ------------------------------------------- SECTION - 1 --------------------------------------------------------------
-# data = []
-# data_series = pd.read_csv(data_dir + 'Roman Urdu DataSet.csv', header=None).iloc[:, 0]
-# preprocessed_data_series = data_series.apply(
-#     lambda l: sub_space(
-#         sub_characters(
-#             sub_quotes(
-#                 sub_initial(l)))))
-#
-# for line in preprocessed_data_series:
-#     if not line.isspace():
-#         data.append(line.strip())
-#
-# np.savetxt(filename,data,fmt='%s')
+data = []
+data_series = pd.read_csv(data_dir + 'Roman Urdu DataSet.csv', header=None).iloc[:, 0]
+preprocessed_data_series = data_series.apply(
+    lambda l: sub_space(
+        sub_characters(
+            sub_quotes(
+                sub_initial(l)))))
+
+for line in preprocessed_data_series:
+    if not line.isspace():
+        data.append(line.strip())
+
+np.savetxt(filename, data, fmt='%s')
 
 # ------------------------------------------- SECTION - 2 --------------------------------------------------------------
-# with open(data_dir + 'counter.txt') as file:
-#     text = file.read()
-#
-# with open('counter_roman_urdu.txt', 'w') as out:
-#     for sentence in sub_nextline(text).split('۔'):
-#         out.write(transliterate(sentence)+'\n')
+with open(data_dir + 'counter.txt') as file:
+    text = file.read()
+
+with open('counter_roman_urdu.txt', 'w') as out:
+    for sentence in sub_nextline(text).split('۔'):
+        out.write(transliterate(sentence) + '\n')
 
 # ------------------------------------------- SECTION - 3 --------------------------------------------------------------
 data_series = pd.read_csv(data_dir + 'imdb_urdu_reviews.csv').iloc[:, 0]
@@ -74,3 +74,23 @@ with open('imdb_urdu_reviews_roman_urdu.txt', 'w') as out:
             out.write(transliterate(sentence) + '\n')
 
 # ------------------------------------------- SECTION - 4 --------------------------------------------------------------
+with open(data_dir + 'uner.txt', encoding="utf16") as file:
+    text = file.read()
+
+with open('uner_roman_urdu.txt', 'w') as out:
+    for sentence in sub_nextline(re.sub(r"</?[A-Z]+>", ' ', text)).split('۔'):
+        out.write(transliterate(sentence) + '\n')
+
+# ------------------------------------------- SECTION - 5 --------------------------------------------------------------
+text = ''
+for f_name in os.listdir(data_dir + 'news/Fake'):
+    with open(data_dir + 'news/Fake/' + f_name) as file:
+        text += ' ' + file.read()
+
+for f_name in os.listdir(data_dir + 'news/Real'):
+    with open(data_dir + 'news/Real/' + f_name) as file:
+        text += ' ' + file.read()
+
+with open('news_roman_urdu.txt', 'w') as out:
+    for sentence in sub_nextline(text).split('۔'):
+        print(transliterate(sentence) + '\n')
