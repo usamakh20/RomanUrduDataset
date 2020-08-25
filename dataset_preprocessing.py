e42626e30bf43b45960ec6465765ab2d079f4eca
usamathescientist <usamathescientist@gmail.com>
2020-08-25 17:31:29 +0500

    Version 1.0

--- dataset_preprocessing.py
+++ dataset_preprocessing.py
@@ -1,6 +1,7 @@
 import re
-import time
 import os
+import time
+import logging
 import numpy as np
 import pandas as pd
 from helper import transliterate
@@ -8,15 +9,9 @@ from helper import transliterate
 data_dir = 'Urdu datasets/'
 regex_chars = '[\'".-]'
 regex_alphabets = '[^a-zA-z]'
-base_filename = 'Roman_Urdu_Sentences.txt'
 timestamp = str(time.time()).replace('.', '')
 
-choice = input('Merge into existing ' + base_filename + ' (y/n): ')
-
-if choice == 'y' or choice == 'Y':
-    filename = base_filename
-else:
-    filename = base_filename + '_' + str(timestamp) + '.txt'
+logging.basicConfig(filename='app.log', filemode='w', format='%(message)s')
 
 
 def sub_initial_urdu(string):
@@ -56,30 +51,33 @@ for line in preprocessed_data_series:
     if not line.isspace():
         data.append(line.strip())
 
-np.savetxt(filename, data, fmt='%s')
+np.savetxt('Roman_Urdu_Sentences.txt', data, fmt='%s')
 
 # ------------------------------------------- SECTION - 2 --------------------------------------------------------------
 with open(data_dir + 'counter.txt') as file:
     text = file.read()
 
 with open('counter_roman_urdu.txt', 'w') as out:
-    for sentence in sub_nextline(text).split('۔'):
+    for index, sentence in enumerate(sub_nextline(text).split('۔')):
         out.write(transliterate(sentence) + '\n')
+        logging.warning('counter_roman_urdu.txt: %d' % index)
 
 # ------------------------------------------- SECTION - 3 --------------------------------------------------------------
 data_series = pd.read_csv(data_dir + 'imdb_urdu_reviews.csv').iloc[:, 0]
 with open('imdb_urdu_reviews_roman_urdu.txt', 'w') as out:
     for data in data_series:
-        for sentence in data.split('۔'):
+        for index,sentence in enumerate(data.split('۔')):
             out.write(transliterate(sentence) + '\n')
+            logging.warning('imdb_urdu_reviews_roman_urdu.txt: %d' % index)
 
 # ------------------------------------------- SECTION - 4 --------------------------------------------------------------
 with open(data_dir + 'uner.txt', encoding="utf16") as file:
     text = file.read()
 
 with open('uner_roman_urdu.txt', 'w') as out:
-    for sentence in sub_nextline(re.sub(r"</?[A-Z]+>", ' ', text)).split('۔'):
+    for index,sentence in enumerate(sub_nextline(re.sub(r"</?[A-Z]+>", ' ', text)).split('۔')):
         out.write(transliterate(sentence) + '\n')
+        logging.warning('uner_roman_urdu.txt: %d' % index)
 
 # ------------------------------------------- SECTION - 5 --------------------------------------------------------------
 text = ''
@@ -92,5 +90,6 @@ for f_name in os.listdir(data_dir + 'news/Real'):
         text += ' ' + file.read()
 
 with open('news_roman_urdu.txt', 'w') as out:
-    for sentence in sub_nextline(text).split('۔'):
-        print(transliterate(sentence) + '\n')
+    for index,sentence in enumerate(sub_nextline(text).split('۔')):
+        out.write(transliterate(sentence) + '\n')
+        logging.warning('news_roman_urdu.txt: %d' % index)
